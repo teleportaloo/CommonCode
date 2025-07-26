2f0275683bff67dc71151155a317ea0bb5bc055f
Andrew Wallace <pdxbus@me.com>
2025-07-26 11:07:48 -0700

    Extracted the prefix and values into separare marcros so can be used by apps directly.

--- DebugCommon.h
+++ DebugCommon.h
@@ -45,10 +45,13 @@ extern void CommonDebugAssert(void);
 #define DEBUG_ON_FOR_FILE ((CommonDebugLogLevel() & DEBUG_LEVEL_FOR_FILE))
 #define DEBUG_AND(X) (DEBUG_ON_FOR_FILE) ? ((X)) : (FALSE)
 
+#define DEBUG_LOG_PREFIX @"<%@:%s:%d> "
+#define DEBUG_LOG_PREFIX_VALS                                                  \
+    CommonDebugLogStr(DEBUG_LEVEL_FOR_FILE), __func__, __LINE__
+
 #define DEBUG_LOG(s, ...)                                                      \
     if (DEBUG_ON_FOR_FILE) {                                                   \
-        NSLog(@"<%@:%s:%d> %@", CommonDebugLogStr(DEBUG_LEVEL_FOR_FILE),       \
-              __func__, __LINE__,                                              \
+        NSLog(DEBUG_LOG_PREFIX @"%@", DEBUG_LOG_PREFIX_VALS,                   \
               [NSString stringWithFormat:(s), ##__VA_ARGS__]);                 \
     }
 #define DEBUG_PRINTF(format, args...)                                          \
@@ -65,8 +68,7 @@ extern void CommonDebugAssert(void);
 
 #define DEBUG_LOG_MAYBE(C, S, ...)                                             \
     if (DEBUG_ON_FOR_FILE && (C)) {                                            \
-        NSLog(@"<%@:%s:%d> %@", CommonDebugLogStr(DEBUG_LEVEL_FOR_FILE),       \
-              __func__, __LINE__,                                              \
+        NSLog(DEBUG_LOG_PREFIX @"%@", DEBUG_LOG_PREFIX_VALS,                   \
               [NSString stringWithFormat:(S), ##__VA_ARGS__]);                 \
     }
 
@@ -96,32 +98,41 @@ extern void CommonDebugAssert(void);
         NSLog(@"**** WARNING **** <%s:%d> %@", __func__, __LINE__,             \
               [NSString stringWithFormat:(s), ##__VA_ARGS__]);                 \
     }
-#define DEBUG_LOG_BOOL(B) DEBUG_LOG(@"%-40s %@", #B, (B) ? @"True" : @"False")
+
+#define DEBUG_ITEM_PREFIX @"%-20s "
+#define DEBUG_LOG_BOOL(B)                                                      \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #B, (B) ? @"True" : @"False")
 #define DEBUG_LOG_CGRect(X)                                                    \
-    DEBUG_LOG(@"%-40s (%g, %g) -> (%g, %g) (%g, %g)", #X, (X).origin.x,        \
-              (X).origin.y, (X).origin.x + (X).size.width,                     \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"(%g, %g) -> (%g, %g) (%g, %g)", #X,          \
+              (X).origin.x, (X).origin.y, (X).origin.x + (X).size.width,       \
               (X).origin.y + (X).size.height, (X).size.width, (X).size.height)
-#define DEBUG_LOG_CGSize(X) DEBUG_LOG(@"%-40s %@", #X, NSStringFromCGSize(X))
-#define DEBUG_LOG_NSString(X) DEBUG_LOG(@"%-40s %@", #X, X)
-#define DEBUG_LOG_CGPoint(X) DEBUG_LOG(@"%-40s %@", #X, NSStringFromCGPoint(X))
-#define DEBUG_LOG_CGFloat(X) DEBUG_LOG(@"%-40s %g", #X, (CGFloat)(X))
-#define DEBUG_LOG_double(X) DEBUG_LOG(@"%-40s %f", #X, (double)(X))
-#define DEBUG_LOG_long(X) DEBUG_LOG(@"%-40s %ld", #X, (long)(X))
-#define DEBUG_LOG_ulong(X) DEBUG_LOG(@"%-40s %lu", #X, (unsigned long)(X))
-#define DEBUG_LOG_longX(X) DEBUG_LOG(@"%-40s 0x%lx", #X, (long)(X))
+#define DEBUG_LOG_CGSize(X)                                                    \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #X, NSStringFromCGSize(X))
+#define DEBUG_LOG_NSString(X) DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #X, X)
+#define DEBUG_LOG_CGPoint(X)                                                   \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #X, NSStringFromCGPoint(X))
+#define DEBUG_LOG_CGFloat(X)                                                   \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%g", #X, (CGFloat)(X))
+#define DEBUG_LOG_double(X) DEBUG_LOG(DEBUG_ITEM_PREFIX @"%f", #X, (double)(X))
+#define DEBUG_LOG_long(X) DEBUG_LOG(DEBUG_ITEM_PREFIX @"%ld", #X, (long)(X))
+#define DEBUG_LOG_ulong(X)                                                     \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%lu", #X, (unsigned long)(X))
+#define DEBUG_LOG_longX(X) DEBUG_LOG(DEBUG_ITEM_PREFIX @"0x%lx", #X, (long)(X))
 #define DEBUG_LOG_NSDate(X)                                                    \
     {                                                                          \
         NSDateFormatter *formatter = [NSDateFormatter new];                    \
         formatter.locale =                                                     \
             [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];              \
         [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZZZ"];             \
-        DEBUG_LOG(@"%-40s %@", #X, [formatter stringFromDate:(X)]);            \
+        DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #X,                                 \
+                  [formatter stringFromDate:(X)]);                             \
     }
-#define DEBUG_LOG_ADDRESS(X) DEBUG_LOG(@"%-40s %p", #X, (X))
-#define DEBUG_LOG_DebugString(X) DEBUG_LOG(@"%-40s %s", #X, (X).DebugString())
+#define DEBUG_LOG_ADDRESS(X) DEBUG_LOG(DEBUG_ITEM_PREFIX @"%p", #X, (X))
+#define DEBUG_LOG_DebugString(X)                                               \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%s", #X, (X).DebugString())
 #define DEBUG_LOG_UIEdgeInsets(X)                                              \
-    DEBUG_LOG(@"%-40s %g, %g, %g, %g", #X, (X).top, (X).left, (X).bottom,      \
-              (X).right)
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%g, %g, %g, %g", #X, (X).top, (X).left,      \
+              (X).bottom, (X).right)
 #define LOG_NSError(error)                                                     \
     if (error) {                                                               \
         ERROR_LOG(@"NSError: %@\n", error.description);                        \
@@ -133,7 +144,8 @@ extern void CommonDebugAssert(void);
     }
 #define DEBUG_FUNC() DEBUG_LOG(@"enter")
 #define DEBUG_FUNCEX() DEBUG_LOG(@"exit")
-#define DEBUG_LOG_description(X) DEBUG_LOG(@"%-40s %@", #X, (X).description)
+#define DEBUG_LOG_description(X)                                               \
+    DEBUG_LOG(DEBUG_ITEM_PREFIX @"%@", #X, (X).description)
 #define DEBUG_LOG_class(X) DEBUG_LOG(@"%s: %s", #X, object_getClassName(X))
 #define DEBUG_LOG_NSIndexPath(I)                                               \
     DEBUG_LOG(@"%s: section %d row %d", #I, (int)((I).section), (int)((I).row));
